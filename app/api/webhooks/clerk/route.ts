9c137f0525a6a52bbf1a54abc394e15d4a3aeec7
deepak-singh19 <deepaksingh23lodhi@gmail.com>
2024-03-10 01:55:56 +0530

    routes import fixed

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -4,8 +4,7 @@ import { WebhookEvent } from "@clerk/nextjs/server";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { Webhook } from "svix";
-
-import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
+import { createUser, updateUser, deleteUser, updateCredits } from "@/lib/actions/user.actions";
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
